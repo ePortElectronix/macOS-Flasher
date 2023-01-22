20d6cbc9e371bb07d0d5a8b56e24f260f8ac1fea
ePortElectronix <85053264+ePortElectronix@users.noreply.github.com>
2023-01-22 07:07:11 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Welcome to the macOS flasher!
+This is a simple shell script to flash macOS to a USB from Mavericks to Ventura. I hope you all find this useful.
+
+  Use Instructions:
+
+      Download this zip or use '$git clone' followed by the url to this repo assuming you haven't done so already.
+      Download your macOS of choice. This script supports Mac OS X Mavericks up to macOS Ventura as of writing this. I plan to update when the next macOS comes out.
+      Rename your target drive or format it with these following names as listed below:
+
+        macOS Ventura = Ventura
+        macOS Monterey = Monterey
+        macOS Big Sur = BigSur
+        macOS Catalina = Catalina
+        macOS Mojave = Mojave
+        macOS High Sierra = HighSierra
+        macOS Sierra = Sierra
+        Mac OS X El Capitan = ElCapitan
+        Mac OS X Yosemite = Yosemite
+        Mac OS X Mavericks = Mavericks
+
+      Open Terminal and cd into the directory. Use '$cd <path to macOS-Flasher>'
+      Once in the directory run '$chmod +x macOS-Flasher.sh' followed by '$./macOS-Flasher.sh'
---
+++ macOS-Flasher.sh
@@ -0,0 +1,10 @@
+sudo /Applications/Install\ macOS\ Ventura.app/Contents/Resources/createinstallmedia --volume /Volumes/Ventura
+sudo /Applications/Install\ macOS\ Monterey.app/Contents/Resources/createinstallmedia --volume /Volumes/Monterey
+sudo /Applications/Install\ macOS\ Big\ Sur.app/Contents/Resources/createinstallmedia --volume /Volumes/BigSur
+sudo /Applications/Install\ macOS\ Catalina.app/Contents/Resources/createinstallmedia --volume /Volumes/Catalina
+sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/createinstallmedia --volume /Volumes/Mojave
+sudo /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/HighSierra
+sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Sierra --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction
+sudo /Applications/Install\ OS\ X\ El\ Capitan.app/Contents/Resources/createinstallmedia --volume /Volumes/ElCapitan --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app
+sudo /Applications/Install\ OS\ X\ Yosemite.app/Contents/Resources/createinstallmedia --volume /Volumes/Yosemite --applicationpath /Applications/Install\ OS\ X\ Yosemite.app --nointeraction
+sudo /Applications/Install\ OS\ X\ Mavericks.app/Contents/Resources/createinstallmedia --volume /Volumes/Mavericks --applicationpath /Applications/Install\ OS\ X\ Mavericks.app --nointeraction
